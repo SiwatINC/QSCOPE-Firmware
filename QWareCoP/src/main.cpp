2f347202ae5342d18e5e87f5c39fd5b5c84acfce
Siwat Sirichai <siwat@siwatinc.com>
2019-11-25 08:58:31 +0700

    QIOP

--- QWareCoP/src/main.cpp
+++ QWareCoP/src/main.cpp
@@ -1,10 +1,47 @@
+// On ESP8266:
+// At 80MHz runs up 57600ps, and at 160MHz CPU frequency up to 115200bps with only negligible errors.
+// Connect pin 12 to 14.
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-SoftwareSerial scope();
+
+#if defined(ESP8266) && !defined(D5)
+#define D5 (14)
+#define D6 (12)
+#define D7 (13)
+#define D8 (15)
+#endif
+
+#ifdef ESP32
+#define BAUD_RATE 57600
+#else
+#define BAUD_RATE 115200
+#endif
+
+// Reminder: the buffer size optimizations here, in particular the isrBufSize that only accommodates
+// a single 8N1 word, are on the basis that any char written to the loopback SoftwareSerial adapter gets read
+// before another write is performed. Block writes with a size greater than 1 would usually fail. 
+SoftwareSerial swSer(5,4);
+
 void setup() {
+	Serial.begin(115200);
+	swSer.begin(115200);
   
+	Serial.println("\nSoftware serial test started");
+
+	for (char ch = ' '; ch <= 'z'; ch++) {
+		swSer.write(ch);
+	}
+	swSer.println("");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	while (swSer.available() > 0) {
+		Serial.write(swSer.read());
+		yield();
+	}
+	while (Serial.available() > 0) {
+		swSer.write(Serial.read());
+		yield();
+	}
+
 }
